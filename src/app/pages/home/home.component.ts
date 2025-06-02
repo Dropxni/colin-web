b464a991e6502038b0d13da676f72498b91a0f56
Dropxni <dropxni@icloud.com>
2025-06-02 02:18:08 -0600

    feat: create Home page and rename program service file to programs.service.ts
    - Implemented Home page component with initial layout and content
    - Renamed program-service.service.ts to programs.service.ts for naming consistency
    - Updated all imports and references to use the new service file name

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, AfterViewInit, Inject, PLATFORM_ID } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProgramsService, Program } from '../../services/programs.service';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
 
+  programs: Program[] = [];
+  private programService = inject(ProgramsService);
+  private router = inject(Router);
+  private platformId = inject(PLATFORM_ID);
+
+  name: string = 'Nuestros Programas';
+
+  constructor() {
+    this.programs = this.programService.obtenerProgramas();
+  }
+
+  goToDetail(id: string) {
+    this.router.navigateByUrl(`/program/${id}`);
+  }
+
+  ngAfterViewInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      const elements = document.querySelectorAll('.fade-in');
+
+      const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+          }
+        });
+      }, { threshold: 0.1 });
+
+      elements.forEach(el => observer.observe(el));
+    }
+  }
 }
